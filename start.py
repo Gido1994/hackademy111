76180e26667f0b1bf224e736eb741dc4cb380b9a
Gido1994 <gilles.domeniconi@student.unisg.ch>
2019-02-14 10:31:54 +0000

    save start

---
+++ start.py
@@ -0,0 +1,15 @@
+
+
+
+
+import code
+print("hello")
+
+num = int(input("Give me a number:    "))
+string = input("Give me a string:    ")
+code.single_row_of(num,string)
+
+print("")
+print("Tak")
+
+
